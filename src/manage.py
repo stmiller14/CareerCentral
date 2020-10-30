dfa04462d9d2fb40af18931ce1fc29ec79c3b10a
stephens-git <stephendmiller14@gmail.com>
2020-10-30 19:56:39 -0400

    sd

--- src/manage.py
+++ src/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobhunt_prod.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src/jobhunt_prod.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
