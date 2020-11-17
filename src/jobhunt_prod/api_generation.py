335ddf1b24240d995a541aa26cbf6c8fc4060c15
stephens-git <stephendmiller14@gmail.com>
2020-11-17 16:04:17 -0500

    Sd

--- src/jobhunt_prod/api_generation.py
+++ src/jobhunt_prod/api_generation.py
@@ -44,7 +44,7 @@ class Api_Response():
 
     def response(self, token, role , location):
         self.errors={'error':'errors'}
-        if token in active_keys.values():
+        if str(token) in active_keys.values():
             ret=Indeed().getrole(role, location)
             return HttpResponse (
                 json.dumps({ 
