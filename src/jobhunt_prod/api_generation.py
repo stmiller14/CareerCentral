9ba940ee1f6c425c212050f5e5abb8064a466b4d
stephens-git <stephendmiller14@gmail.com>
2020-11-17 16:34:55 -0500

    class

--- src/jobhunt_prod/api_generation.py
+++ src/jobhunt_prod/api_generation.py
@@ -9,40 +9,43 @@ from .scrape import multiprocess_simply, multithread_simply, async_monster, buil
 from django.contrib.auth.models import User
 import secrets
 
-active_keys={'stephendmiller14@gmail.com' : '123'}
 
 
 class Generate_Token():
-    global active_keys
+    active_keys={'stephendmiller14@gmail.com' : '123'}
+
     def __init__(self, email): 
         self.key=None
         self.email=email
         self.errors={}
-        
+    
     def activate_key(self):
         token=secrets.token_urlsafe(10)
-        active_keys[self.email]=token
-        print('token is ' , token , self.email)
-        print('all keys ', active_keys)
+        self.addtoactive(self.email, token)
         return token
 
     def is_valid_key(self, key):
         is_valid=False
-        if key in active_keys.values():
+        if key in self.getallkeys():
             is_valid=True
         return is_valid
 
-    def getkey(self, email) :
-        return active_keys[self.email]
 
+    @classmethod
+    def addtoactive(cls, email, token):
+        cls.active_keys[email]=token
+        print('all keys ', cls.active_keys)
+    @classmethod
+    def getallkeys(cls):
+        return cls.active_keys
+    
     def error_handler(self):
         self.errors={"1" : "error"}
         return HttpResponse(self.errors)
 
-
 class Api_Response():
-    global active_keys
     def response(self, token, role , location):
+        active_keys=Generate_Token('api').getallkeys()
         self.errors={'error':'errors'}
         print('the active keys are ' ,  active_keys)
         if str(token) in active_keys.values():
